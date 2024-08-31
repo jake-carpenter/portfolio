1f75c8826695d7807ca778a7d779c79e08a4ffc6
Jake Carpenter <jake@jakecarpenter.dev>
2024-08-31 15:24:47 -0600

    Fix URLs in XML feed

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,7 +2,7 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://jake-carpenter-portfolio.vercel.app/';
+export const siteBaseUrl = 'https://jake-carpenter-portfolio.vercel.app';
 
 export const keywords = [];
 
